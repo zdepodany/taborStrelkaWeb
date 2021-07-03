4e7139aff84d9744321be634ce01d17eac178a02
Zdenek Podany <zdepodany@gmail.com>
2021-07-03 20:22:37 +0200

    improve gallery UI, add modal
    better gallery UI of photos
    add modals (in, out)

---
+++ taborapp/static/js/scripts.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById('modalBG').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('modalBG').style.display = 'none';
+}
