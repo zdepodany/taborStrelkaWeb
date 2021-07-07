09cf1d87ba8b80e2c0bb8bf1c997c10aa7e74b2a
Marcel Plch <marcel.plch@protonmail.com>
2021-07-08 00:41:35 +0200

    Enable admin login
    * Add url to admin login page
    * delete endline sign, where it might not do good

--- taborapp/urls.py
+++ taborapp/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import include, url
 from taborapp.views import index, gallery, adminLogin, adminPanel
 
-# TODO: admin needs only one view, we keep two just because it was easier to create
-
 urlpatterns = [
     url(r'^$', index),
     url(r'^gallery/', gallery),
-    url(r'^adminPanel/', adminPanel)
-    url(r'^adminLogin/', adminLogin),
+    url(r'^admin/', admin),
 ]
 
