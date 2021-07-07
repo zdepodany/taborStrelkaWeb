d1f8c9d621451e7e297ff563e767ba903239815c
Marcel Plch <marcel.plch@protonmail.com>
2021-07-08 00:45:16 +0200

    Implement [admin] login/logout
    A user is now able to log in and out. This includes tabor admin.

--- taborapp/views.py
+++ taborapp/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from pprint import pprint
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {"page":0})
+    return render(request, "index.html", {})
 
 def gallery(request):
     args = request.GET
@@ -13,8 +13,31 @@ def gallery(request):
 
     return render(request, "gallery.html", {"page":1, "year":year})
 
-def adminPanel(request):
-    return render(request, "adminPanel.html", {"page":3})
+def admin(request):
+    username = None
+    session = request.session
 
-def adminLogin(request):
-    return render(request, "adminLogin.html", {"page":2})
+    if request.user.is_authenticated:
+        args = request.GET
+        out = args.get("logout", None)
+        if out:
+            logout(request)
+        else:
+            username = session["username"]
+
+    else:
+        args = request.POST
+        username = args.get("username", None)
+        password = args.get("password", None)
+
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                session["username"] = username
+            else:
+                # TODO: make this return an invalid login info
+                pass
+
+    return render(request, "admin.html", {"user":username})
+
