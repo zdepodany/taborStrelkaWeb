3a55dce30ac848c7200f7aa3e4d8da7c103ecbf7
Marcel Plch <marcel.plch@protonmail.com>
2021-07-08 03:11:41 +0200

    Implement file upload
    Just one file. Multiple is job for tomorrow.
    
    I mean, you can see commit timestamp.

--- taborapp/views.py
+++ taborapp/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
+from .forms import UploadFileForm
+# TODO:
+#
+# 1) It is a good practice to handle forms with
+#    form classes and database models. Do that.
+
+# Helper functions
+# TODO: When there are too many, move them somewhere else
+def handle_uploaded_file(f):
+    with open('tabor.png', 'wb') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            handle_uploaded_file(file)
+            return HttpResponseRedirect('/admin/')
+    else:
+        return HttpResponseRedirect('/')
 
 # Create your views here.
 
@@ -16,15 +38,26 @@ def gallery(request):
 def admin(request):
     username = None
     session = request.session
+    form = UploadFileForm()
 
+    # Do things that require admin auth
     if request.user.is_authenticated:
+        # Validate user
+        username = session["username"]
+        if username != "podany":
+            return HttpResponse("Unauthorized", status=401)
+
+        # Handle logging out
         args = request.GET
         out = args.get("logout", None)
         if out:
             logout(request)
-        else:
-            username = session["username"]
+            return render(request, "admin.html", {"username":None})
 
+        # Handle file upload
+        upload_file(request)
+
+    # Provide login form and handle login
     else:
         args = request.POST
         username = args.get("username", None)
@@ -37,7 +70,7 @@ def admin(request):
                 session["username"] = username
             else:
                 # TODO: make this return an invalid login info
-                pass
+                return HttpResponseRedirect('/admin/?login=-1')
 
-    return render(request, "admin.html", {"user":username})
+    return render(request, "admin.html", {"username":username, "form":form})
 
