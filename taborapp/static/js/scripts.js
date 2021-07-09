21fc33951fdb9833f96c49a6343c77ede31fda20
Zdenek Podany <zdepodany@gmail.com>
2021-07-09 22:33:09 +0200

    Implement browsing photos in modal

--- taborapp/static/js/scripts.js
+++ taborapp/static/js/scripts.js
@@ -1,7 +1,64 @@
-function openModal() {
-    document.getElementById('modalBG').style.display = 'flex';
+// OPENING & CLOSING MODAL WINDOW FOR PHOTO GALLERY
+
+function openModal(index, max) {
+    let imgTag;
+    let modal;
+    let photoWrap;
+    let strIndex;
+    modal = document.getElementById('modalBG');
+    modal.style.display = 'flex';
+
+    strIndex = 'photo'.concat(index.toString())
+    imgTag = document.getElementById(strIndex);
+    photoWrap = document.getElementById('photoWrap');
+    photoWrap.state = new Object();
+    photoWrap.src = imgTag.src;
+    photoWrap.state.index = index;
 }
 
 function closeModal() {
-    document.getElementById('modalBG').style.display = 'none';
+    let photoWrap;
+    let modal;
+    modal = document.getElementById('modalBG');
+    modal.style.display = 'none';
+
+    photoWrap = document.getElementById('photoWrap');
+    photoWrap.src = '';
+}
+
+// LISTING IN MODAL
+
+function modalPrev() {
+    let photoWrap;
+    let index;
+    let strIndex;
+    let ourPhoto;
+
+    photoWrap = document.getElementById('photoWrap');
+    index = photoWrap.state.index;
+
+    if (index > 0) {
+        index--;
+        strIndex = 'photo'.concat(index.toString());
+        ourPhoto = document.getElementById(strIndex);
+        photoWrap.src = ourPhoto.src;
+        photoWrap.state.index = index;
+    }
+}
+
+function modalNext(max) {
+    let photoWrap;
+    let index;
+    let strIndex;
+    let ourPhoto;
+
+    photoWrap = document.getElementById('photoWrap');
+    index = photoWrap.state.index;
+    if (index < max) {
+        index++;
+        strIndex = 'photo'.concat(index.toString());
+        ourPhoto = document.getElementById(strIndex);
+        photoWrap.src = ourPhoto.src;
+        photoWrap.state.index = index;
+    }
 }
