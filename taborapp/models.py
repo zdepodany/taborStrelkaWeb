4966d8db7067f811b0301a416318c945b9bb8c82
Marcel Plch <marcel.plch@protonmail.com>
2021-07-09 18:47:19 +0200

    Implement gallery view

--- taborapp/models.py
+++ taborapp/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class PhotoModel(models.Model):
-    file = models.FileField(upload_to="taborapp/userdata/photos/%Y/%m/%d/")
+    file = models.ImageField(upload_to="photos/%Y/%m/%d/")
 
