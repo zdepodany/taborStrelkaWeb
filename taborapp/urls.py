3a70239f36ea8693ccc7f1d60b725664f83a5106
zdepodany <zdepodany@gmail.com>
2022-02-15 00:21:28 +0100

    Add login page

--- taborapp/urls.py
+++ taborapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, re_path
 # TODO: Port all to classes
-from taborapp.views import index, gallery, AdminView
+from taborapp.views import index, gallery, AdminView, LoginView
 
 urlpatterns = [
     re_path(r'^$', index),
     re_path(r'^gallery/', gallery),
     re_path(r'^admin/', AdminView.as_view()),
+    re_path(r'^login/', LoginView.as_view()),
 ]
 
