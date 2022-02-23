a38db3a257d097d65d46e48048feab37cb6737ca
zdepodany <zdepodany@gmail.com>
2022-02-23 18:24:57 +0100

    Style "Browse" photos button on AdminHub

---
+++ taborapp/static/js/fileUpload.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('#fakeFileUploadButton').forEach(function (button) {
+    let hiddenInput = button.parentElement.querySelector('#realFileUpload');
+    let label = button.parentElement.querySelector('#fakeFileUploadText');
+    let defaultLabelText = 'Nevybráno';
+
+    // Set default text for label
+    label.textContent = defaultLabelText;
+    label.title = defaultLabelText;
+
+    // Click on hidden "Browse button"
+    button.addEventListener('click', function () {
+        hiddenInput.click();
+    });
+
+    // Show number of chosen files
+    hiddenInput.addEventListener('change', function () {
+        let fileNumberList = hiddenInput.files.length;
+        label.textContent = 'Vybráno fotek: ' + fileNumberList.toString();
+    })
+
+});
